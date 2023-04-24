63b71489d4e43a2d87101a945bb80055646aa44b
Тимур <selimkhanov.t20fpm@fpm.dnu.edu.ua>
2023-04-24 10:08:41 +0300

    Revert "Yes"
    This reverts commit f609c66a667b24bbfe59522c67543b5e8164b136.

--- DB_stage1.2/Models/Genre.cs
+++ DB_stage1.2/Models/Genre.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace DB_stage1._2.Models;
 
 public partial class Genre
 {
     public int Id { get; set; }
-    [Required(ErrorMessage = "Поле порожнє.")]
-    [Display(Name = "Жанр")]
-    public string Name { get; set; }
+
+    public string? Name { get; set; }
 
     public virtual ICollection<Exhibition> Exhibitions { get; set; } = new List<Exhibition>();
 }
--- DB_stage1.2/Program.cs
+++ DB_stage1.2/Program.cs
@@ -1,14 +1,7 @@
-using DB_stage1._2.Models;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<BdgalleryContext>(option => option.UseSqlServer(
-builder.Configuration.GetConnectionString("DefaultConnection")
-));
 
 var app = builder.Build();
 
@@ -29,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Genres}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
